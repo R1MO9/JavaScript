9f417c1f76d16e11082076a8be77faf6db0a464e
Rupankar Das <rupankardasofficial@gmail.com>
2024-02-19 18:51:15 +0530

    Datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treats all js codde as a newer version of js
+
+//this can be run in a browser but not in node
+/* alert("Your PC is infected 
+with a virus. Click OK to remove the virus.") */
+
+let name = "Rupankar" //String
+let age = 20 //Number
+let isApproved = true //Boolean
+let state//Undefined
+let temp = null //Null
+//Symbol
+//Object
+
+console.log(typeof null); //Object
+console.log(typeof undefined); // undefined
