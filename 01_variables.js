82c84a68acc43e49d252b5f14a4fe6450e9de7b9
Rupankar Das <rupankardasofficial@gmail.com>
2024-02-19 18:27:47 +0530

    Variables

---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+//const - can't be changed ever
+const accountId = 70369027
+
+//Prefer not to use var because of issue in block scope and function scope
+let email= "javascript@gmail.com" 
+var accountPassword = "123456"
+
+//Value is undefined
+let accoutState
+
+// Prefer not to use this also
+accountCity= "New York"
+
+console.table([accountId, accountPassword, email, accountCity])
