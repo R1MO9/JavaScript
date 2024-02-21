a217a6f20df809151c07f295e8647086917279f2
Rupankar Das <rupankardasofficial@gmail.com>
2024-02-21 13:31:56 +0530

    Comparison

---
+++ 04_comparison.js
@@ -0,0 +1,14 @@
+console.log(2 > 1)
+console.log(2 >= 1)
+console.log(2 < 1)
+console.log(2 == 1)
+console.log(2 != 1)
+
+console.log("2" > 1)
+console.log("02" > 1)
+
+console.log(null < 1)
+
+
+console.log("2" == 2); //true
+console.log("2" === 2);//False Strict Comparision it also checks the data type
