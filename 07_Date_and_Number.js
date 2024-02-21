9b4d69e371f88016cac96db06c799199e960312d
Rupankar Das <rupankardasofficial@gmail.com>
2024-02-21 17:38:28 +0530

    Date and Time

---
+++ 07_Date_and_Number.js
@@ -0,0 +1,16 @@
+const myDate = new Date() 
+console.log(myDate);
+console.log(myDate.toDateString());
+
+console.log(myDate.getFullYear());  //Get year as a four digit number (yyyy)
+console.log(myDate.getMonth());  //Get month as a number (0-11)
+console.log(myDate.getDate());  //Get day as a number (1-31)
+console.log(myDate.getDay());   //Get weekday as a number (0-6)
+console.log(myDate.getHours());  //Get hour (0-23)
+console.log(myDate.getMinutes());   //Get minute (0-59)
+console.log(myDate.getSeconds());   //Get second (0-59)
+console.log(myDate.getMilliseconds());  //Get millisecond (0-999)
+console.log(myDate.getTime());  //Get time (milliseconds since January 1, 1970)
+
+
+
