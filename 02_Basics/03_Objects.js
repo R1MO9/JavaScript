3cb7c836fdbaf6be5143a91d0c06bf4a57b86048
Rupankar Das <rupankardasofficial@gmail.com>
2024-02-26 00:04:45 +0530

    Objects

---
+++ 02_Basics/03_Objects.js
@@ -0,0 +1,28 @@
+const mySym = Symbol("key1")
+
+const user = {
+    name: "RIMON",
+    age : "20,",
+    location : "Kolkata",
+    isLoggedIn: false,
+    email : "rimon@google.com",
+    [mySym] : "17023912739"
+}
+
+console.log(user.name);
+console.log(user["email"]);
+console.log(user[mySym]);
+
+//user["name"] = "abdnf"
+//console.log(user["name"]);
+
+// Object.freeze(user)
+console.log(user);
+
+//user["name"] = "abcdef"
+//console.log(user["name"]);
+
+user.greeting = function() {
+    console.log(`Hello JavaScript user, ${this.name}`);
+}
+console.log(user.greeting());
