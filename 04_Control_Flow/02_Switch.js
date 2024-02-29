1e44593dcb9213e06cdfc169cf34f3a80a32bfe5
Rupankar Das <rupankardasofficial@gmail.com>
2024-02-29 13:02:31 +0530

    Switch Case

---
+++ 04_Control_Flow/02_Switch.js
@@ -0,0 +1,29 @@
+// switch(key){
+//     case 1:
+//         console.log('One');
+//         break;
+//     case 2:
+//         console.log('Two');
+//         break;
+//     default:
+//         console.log('Other');
+//         break;
+// }
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log('January');
+        break;
+    case 2:
+        console.log('February');
+        break;
+    case 3:
+        console.log('March');
+        break;
+    case 4:
+        console.log('April');
+        break;
+    default:
+        console.log('Other');
+}
