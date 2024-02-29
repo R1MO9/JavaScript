c94b8cdb6e11d5fb572366a134ab9a968abdd7aa
Rupankar Das <rupankardasofficial@gmail.com>
2024-02-29 13:02:05 +0530

    If Else & Operators

--- 04_Control_Flow/01_If_Else.js
+++ 04_Control_Flow/01_If_Else.js
@@ -1,5 +1,5 @@
 //Operators
-// <, >, <=, >=, ==, !=, ===, !==
+// <, >, <=, >=, ==, !=, ===, !==, &&, ||, !, ?
 // == checks only value
 // === checks value and data type
 
@@ -11,3 +11,25 @@ if(a <= 2) {
     console.log("a is greater than 2");
 }
 
+// Nullish Coalescing Operator (??) : null or undefined
+let val1;
+val1 = 5 ?? 10;
+console.log(val1); // 5
+
+let val2;
+val2 = null ?? 10;
+console.log(val2); // 10
+
+let val3;
+val3 = undefined ?? 15;
+console.log(val3); // 15
+
+let val4;
+val4 = null ?? 0 ?? 20;
+console.log(val4); // 0
+
+// Ternary Operator
+// condition ? true : false
+let age = 18;
+let status = (age >= 18) ? "Adult" : "Child";
+console.log(status); // Adult
