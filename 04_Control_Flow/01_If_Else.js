3dfab7e250832121777d5db46e1300148e9df110
Rupankar Das <rupankardasofficial@gmail.com>
2024-02-29 10:41:31 +0530

    If Else

---
+++ 04_Control_Flow/01_If_Else.js
@@ -0,0 +1,13 @@
+//Operators
+// <, >, <=, >=, ==, !=, ===, !==
+// == checks only value
+// === checks value and data type
+
+// if else
+a = 2;
+if(a <= 2) {
+    console.log("a is less than or equal to 2");
+} else {
+    console.log("a is greater than 2");
+}
+
